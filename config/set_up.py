1cd2595bdd5df72755b803bc992e35d05a1ab6bf
Ernest Kwame Amuzu <57227547+Calliernie@users.noreply.github.com>
2021-12-02 16:25:42 +0000

    disabled console logging of errors

--- config/set_up.py
+++ config/set_up.py
@@ -5,5 +5,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 class Setup:
     @staticmethod
     def get_driver():
-        driver = webdriver.Chrome(ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        options.add_argument("--log-level=3")
+        driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
         return driver
